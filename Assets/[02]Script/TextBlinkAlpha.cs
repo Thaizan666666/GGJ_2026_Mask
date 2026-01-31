0638d4d4bc2dec51f31671bc4dd7612cbd27b54c
Thaizan666666 <Thaizan246810@gmail.com>
2026-01-31 14:04:04 +0700

    Update_button
    Even_button

---
+++ Assets/[02]Script/TextBlinkAlpha.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TextBlinkAlpha : MonoBehaviour
+{
+    public TextMeshProUGUI textMesh;
+    public float blinkSpeed = 1f; // ความเร็วในการกระพลิบ
+    public float minAlpha = 0f;   // ค่า Alpha ต่ำสุด (0 = โปร่งใสสนิท)
+    public float maxAlpha = 1f;   // ค่า Alpha สูงสุด (1 = ทึบสนิท)
+    public bool isBlink = true;
+    void Start()
+    {
+        if (textMesh == null)
+            textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        // ใช้ PingPong เพื่อให้ค่าสลับไปมาอัตโนมัติ
+        if (isBlink)
+        {
+            float alpha = Mathf.PingPong(Time.time * blinkSpeed, maxAlpha - minAlpha) + minAlpha;
+
+            Color color = textMesh.color;
+            color.a = alpha;
+            textMesh.color = color;
+        }
+    }
+}
